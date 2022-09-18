250bc408c16693fedf046067a650976f3e503033
icaro paiva <icaro.paiva@acct.global>
2022-09-17 22:02:01 -0300

    socket server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,39 @@
 import express from "express"
-import { Request, Response } from "express"
+import { createServer } from "http"
+import { Server } from "socket.io"
 
 const app = express()
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Application works!")
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*"
+  }
 })
 
-app.listen(3000, () => {
+const players: {
+  id: string
+  name: string
+}[] = []
+
+io.on("connection", socket => {
+  const { id } = socket
+
+  // push player
+  socket.on("name", (name: string) => {
+    players.push({ id, name })
+    io.emit("players", players)
+  })
+
+  // remove player
+  socket.on("disconnect", () => {
+    players.splice(
+      players.findIndex(player => player.id === id),
+      1
+    )
+    io.emit("players", players)
+  })
+})
+
+httpServer.listen(3000, () => {
   console.log("Application started on port 3000!")
 })
