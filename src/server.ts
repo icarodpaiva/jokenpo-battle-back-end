13c126811460ee9325813152cc5c472f17ed6f5c
icaro paiva <icaro.paiva@acct.global>
2022-09-18 17:40:29 -0300

    tournment brackets created

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { createServer } from "http"
 import { Server } from "socket.io"
+import { Players, TourmentBrackets } from "./types/global"
 
 const PORT = process.env.PORT || 3000
 
@@ -12,45 +13,25 @@ const io = new Server(httpServer, {
   }
 })
 
-//  players
-const players: {
-  id: string
-  name: string
-}[] = []
+const players: Players[] = []
+const tournment_brackets: TourmentBrackets[][] = []
+let phase = 0
 
 const updatePlayersList = () => io.emit("players", players)
+const updateTournmentBrackets = () =>
+  io.emit("tournment_brackets", tournment_brackets)
 
 // socket events
 io.on("connection", socket => {
   const { id } = socket
 
-  // push player
-  socket.on("name", (name: string) => {
+  // player connect
+  socket.on("player_connect", (name: string) => {
     players.push({ id, name })
-
-    // tournment
-    const tournment_brackets =
-      players.length % 2 === 0 || players.length <= 1 ? [] : [[]]
-
-    let playersInBracket = players.length
-    while (playersInBracket >= 1) {
-      tournment_brackets.push([])
-      playersInBracket = playersInBracket / 2
-    }
-
-    // first row
-    for (let i = 0; i < players.length; i++) {
-      tournment_brackets[0].push(players[i])
-    }
-
-    console.log("players total", players.length)
-    console.log("playersInBracket", playersInBracket)
-    console.log("tournment_brackets", tournment_brackets)
-
     updatePlayersList()
   })
 
-  // remove player
+  // player disconnect
   socket.on("disconnect", () => {
     players.splice(
       players.findIndex(player => player.id === id),
@@ -59,8 +40,45 @@ io.on("connection", socket => {
     updatePlayersList()
   })
 
-  // tournment brackets
-  // io.emit("tournment_brackets", tournment_brackets)
+  // tournment start
+  socket.on("tournment_start", () => {
+    // prevent errors with odd quantity players
+    if (players.length % 2 !== 0 && players.length > 1) {
+      tournment_brackets.push([])
+    }
+
+    // create empty objects with correct quantity for each phase tournment
+    let playersInBracket = players.length
+    while (playersInBracket >= 1) {
+      tournment_brackets.push(
+        Array.from(Array(Math.ceil(playersInBracket / 2)), () => ({}))
+      )
+      playersInBracket = playersInBracket / 2
+    }
+
+    // fill in brackets on the phase 0
+    for (let i = 0; i < players.length; i++) {
+      tournment_brackets[phase][i] = { ...players[i], winner: null }
+    }
+
+    updateTournmentBrackets()
+  })
+
+  // tournment phases
+  // change phase when the phase is over
+  // if (
+  //   tournment_brackets[phase].every(item => item.winner !== null) &&
+  //   tournment_brackets[phase + 1]
+  // ) {
+  //   phase++
+  // }
+
+  // console.log("tournment_brackets", tournment_brackets)
+  // console.log("phase", phase)
+
+  // console.log("players total", players.length)
+  // console.log("playersInBracket", playersInBracket)
+  // console.log("tournment_brackets", tournment_brackets)
 })
 
 app.use("/", (_, res) => {
---
+++ src/types/global.ts
@@ -0,0 +1,10 @@
+export interface Players {
+  id: string
+  name: string
+}
+
+export interface TourmentBrackets {
+  id?: string
+  name?: string
+  winner?: boolean | null
+}
